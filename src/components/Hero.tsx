a49f7bc2763d1b759dc5c885bea288f0aa635b96
MoeSoftware <not.pankakes@gmail.com>
2025-05-26 17:03:07 -0700

    change path

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,7 +8,7 @@ const Hero = () => {
       <div
         className="absolute inset-0 bg-cover bg-center"
         style={{
-          backgroundImage: "url('/public/assets/GSH&Clogo.jpg')",
+          backgroundImage: "url('/assets/GSH&Clogo.jpg')",
           backgroundPosition: "center 25%",
         }}
       ></div>
